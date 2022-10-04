38775403be39599a46760a260e95d9d6053463c5
A_Devops_Guy <98376417+iamkishore0@users.noreply.github.com>
2022-10-04 11:09:25 +0530

    Create setup.sh

---
+++ Terraform/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+sudo yum update -y
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install terraform
